ecda71804165a676e27e9575bf2c2de260a66c9a
Igor Saric <igor.saric@outlook.com>
2022-12-28 01:02:56 +0100

    a

--- model.js
+++ model.js
@@ -112,7 +112,7 @@ const buildDataModel = (requestModel) => {
 
   // enrich model
   model.invoiceNumber = fin;
-  model.invoiceDate = mine ? now.subtract(3, 'hour').format(fullDateFormat) : now.format(fullDateFormat);
+  model.invoiceDate = mine ? now.subtract(6, 'hour').format(fullDateFormat) : now.format(fullDateFormat);
   model.placeOfIssue = `${model.sellerCity}, ${model.sellerCountry}`;
   model.deliveryDate = eom.format(shortDateFormat);
   model.dueDate = eom.add(15, 'day').format(shortDateFormat);
