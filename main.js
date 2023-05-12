c6527fb90761fa918bbf4704c49d7c4e7de36f1e
Igor Saric <igor.saric@outlook.com>
2023-05-12 21:10:42 +0200

    a

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
