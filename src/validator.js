11fce875ed59d7ef2f62519ba38f0b831e7d5f0e
Igor Saric <igor.saric@outlook.com>
2023-11-15 02:31:43 +0100

    a

--- src/validator.js
+++ src/validator.js
@@ -17,26 +17,47 @@ const isValidDecimal = (input) => {
 };
 
 const validator = new Validator();
+
 validator.customFormats.fmtDecimal = (input) => isValidDecimal(input);
 validator.customFormats.fmtString = (input) => isValidString(input);
 validator.customFormats.fmtInteger = (input) => isValidInteger(input);
-validator.customFormats.fmtUrl = (input) => isValidString(input) && input.startsWith('https://');
+validator.customFormats.fmtUrl = (input) => (input === null) || (isValidString(input) && input.startsWith('https://'));
 validator.customFormats.fmtYear = (input) => isValidInteger(input) && (input >= 2001) && (input <= 2100);
 validator.customFormats.fmtMonth = (input) => isValidInteger(input) && (input >= 1) && (input <= 12);
 
-const getErrorMessage = (argument) => {
-  if (!argument) {
+const getError = (e) => {
+  if (!e.property && !e.argument) {
     return null;
   }
-  const messageMap = {
-    fmtDecimal: `Must be a positive number, and can have at most 2 decimal places.`,
-    fmtString: `Must be non-empty and have less than ${maxStringLength} characters.`,
-    fmtInteger: `Must be a positive integer.`,
-    fmtUrl: `Must be non-empty, have less than ${maxStringLength} characters and start with https://.`,
-    fmtYear: `Must be a positive integer between 2001 and 2100.`,
-    fmtMonth: `Must be a positive integer between 1 and 12.`,
-  };
-  return messageMap[argument];
+  const prop = e.property.replace(/^instance\.?/, '');
+  if (e.name === 'format') {
+    const fmtMessages = {
+      fmtDecimal: `${prop} must be a positive number, and can have at most 2 decimal places.`,
+      fmtString: `${prop} must be a non-empty string and have less than ${maxStringLength} characters.`,
+      fmtInteger: `${prop} must be a positive integer.`,
+      fmtUrl: `${prop} must be a non-empty string, have less than ${maxStringLength} characters and start with https://.`,
+      fmtYear: `${prop} must be a positive integer between 2001 and 2100.`,
+      fmtMonth: `${prop} must be a positive integer between 1 and 12.`
+    };
+    return {
+      field: prop,
+      message: fmtMessages[e.argument]
+    };
+  }
+  if (e.name === 'required') {
+    const field = prop ? `${prop}.${e.argument}` : e.argument;
+    return {
+      field: field,
+      message: `${field} is a required field.`
+    };
+  }
+  if (e.name === 'type' && e.message) {
+    return {
+      field: prop,
+      message: `${prop} ${e.message.replace('type(s)', 'type')}`
+    };
+  }
+  return null;
 };
 
 const validate = (model) => {
@@ -44,20 +65,16 @@ const validate = (model) => {
   const errors = [];
   if (result.errors) {
     for (let i = 0; i < result.errors.length; i++) {
-      const e = result.errors[i];
-      const message = getErrorMessage(e.argument);
-      if (e.property && message) {
-        errors.push({
-          field: e.property.substring(e.property.indexOf('.') + 1), // remove "instance."
-          message: message
-        });
+      const err = getError(result.errors[i]);
+      if (err && !errors.some(x => x.field === err.field)) {
+        errors.push(err);
       }
     }
   }
   if (!result.valid && errors.length === 0) {
     errors.push({
       field: null,
-      errors: 'Model is not valid (unexpected error).'
+      message: 'Model is not valid (unexpected error).'
     });
   };
   return {
