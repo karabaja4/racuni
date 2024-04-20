0aafc4b6fc75e488de3b2c1dc4c04a94f03ab0fc
Igor Saric <igor.saric@outlook.com>
2024-04-20 21:06:15 +0200

    a

--- src/validator.js
+++ src/validator.js
@@ -110,7 +110,7 @@ const validate = (model) => {
   if (!model.items || !Array.isArray(model.items) || (model.items.length === 0)) {
     push('items', 'must be a non-empty array.');
   } else if (model.items.length > 5) {
-    push('items', 'must have at most 5 elements.');
+    push('items', 'can have at most 5 elements.');
   } else {
     for (let i = 0; i < model.items.length; i++) {
       const item = model.items[i];
