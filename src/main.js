55529ec55085127ac354039f92bd700f5a840877
Igor Saric <karabaja4@gmail.com>
2024-08-04 00:00:03 +0200

    fix chrome 127

--- src/main.js
+++ src/main.js
@@ -84,7 +84,7 @@ app.post('/generate', async (request, response) => {
     log.info(`Generating (${jsonHash}): ${json}`);
     const browser = await puppeteer.launch({
       executablePath: '/usr/bin/chromium-browser',
-      headless: true
+      headless: 'shell'
     });
   
     const page = await browser.newPage();
