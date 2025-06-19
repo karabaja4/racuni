985e8373bfb76b7ca61d8c1fb1c2e3e38c1211fe
Igor Saric <karabaja4@gmail.com>
2025-06-19 12:12:47 +0200

    a

--- src/main.js
+++ src/main.js
@@ -73,7 +73,6 @@ app.post('/generate', async (request, response) => {
   log.info(`Generating (${jsonHash}): ${json}`);
   const browser = await puppeteer.launch({
     executablePath: '/usr/bin/chromium-browser',
-    headless: 'shell',
     args: [ '--disable-gpu' ]
   });
 
